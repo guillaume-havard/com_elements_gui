e585bc4adcdf043378d4cec3d72c53c7da426d33
Guillaume Havard <guill.havard@gmail.com>
2014-12-19 15:51:46 +0100

    Add command to widget (excepting power_entry)

--- src/element.py
+++ src/element.py
@@ -10,6 +10,12 @@ import tkinter as tk
 
 class Element(tk.Frame):
 
+    # internal State
+    is_paired = True
+    MIN_POWER = 0
+    MAX_POWER = 255
+    status_old = 1
+    
     def __init__(self, master, pos):
         tk.Frame.__init__(self, master)
         self.grid(row=pos)
@@ -18,8 +24,8 @@ class Element(tk.Frame):
         self.id.set("ID")
         self.status = tk.IntVar()
         self.status.set(1)
-        self.power = tk.IntVar()
-        self.power.set(1)
+        self.power = tk.StringVar()
+        self.power.set(0)
         self.posx = tk.StringVar()
         self.posx.set("X")
         self.posy = tk.StringVar()
@@ -35,17 +41,25 @@ class Element(tk.Frame):
         self.status_frame.grid(row=0, column=1)
         self.status_on_radio = tk.Radiobutton(self.status_frame, text="ON", 
                                               variable=self.status,
+                                              command=self.status_cmd,
                                               value=1)
         self.status_on_radio.grid(row=0, column=0)             
         self.status_off_radio = tk.Radiobutton(self.status_frame, text="OFF", 
                                               variable=self.status,
+                                              command=self.status_cmd,
                                               value=0)
         self.status_off_radio.grid(row=1, column=0)          
         # Power
-        self.power_entry = tk.Entry(self, textvariable=self.power, width=3)
-        self.power_entry.grid(row=0, column=2)        
-        self.power_scale = tk.Scale(self, variable=self.power, from_=0, to=255,
-                                    orient=tk.HORIZONTAL)
+        okay_entry = self.register(self.power_entry_cmd)
+        self.power_entry = tk.Entry(self, textvariable=self.power, width=3,
+                                    validatecommand=(okay_entry, '%P'),
+                                    validate='key')
+        self.power_entry.grid(row=0, column=2)  
+        self.power_entry["state"]=tk.DISABLED      
+        self.power_scale = tk.Scale(self, variable=self.power, 
+                                    from_=self.MIN_POWER, to=self.MAX_POWER,
+                                    orient=tk.HORIZONTAL,
+                                    command=self.power_scale_cmd)
         self.power_scale.grid(row=0, column=3)        
         # Posx
         self.posx_label = tk.Label(self, textvariable=self.posx)
@@ -57,9 +71,45 @@ class Element(tk.Frame):
         self.date_label = tk.Label(self, textvariable=self.date)
         self.date_label.grid(row=0, column=6)
         # Pairing button 
-        self.pair_button = tk.Button(self, text="unappair")
+        self.pair_button = tk.Button(self, text="unappair",
+                                     command=self.pair_cmd)
         self.pair_button.grid(row=0, column=7)
+    
+    
+    def status_cmd(self):
+        if self.status_old and not self.status.get():
+            print("switch off")
+            self.power_scale["state"]=tk.DISABLED 
+        elif not self.status_old and self.status.get():
+            print("switch on")  
+            self.power_scale["state"]=tk.ACTIVE 
         
+        self.status_old = self.status.get()      
+        
+    def power_entry_cmd(self, value):        
+        if value == "":
+            value = "0"
+        elif not value.isdigit():
+            return False
+        
+        value = str(int(value))
+        #Limit verification with power_scale        
+        self.power.set(value)
+        
+        
+        return True
+        
+    def power_scale_cmd(self, value):
+        # Fait avec l'entry
+        print("  ++", self.power.get(), value)
+        pass
+        
+    def pair_cmd(self):
+        if self.is_paired:
+            self.unappair()
+        else:
+            self.appair()
+            
     def unappair(self):
         self.status_frame.grid_remove()
         self.power_scale.grid_remove()
@@ -69,6 +119,8 @@ class Element(tk.Frame):
         self.date_label.grid_remove()
         self.pair_button["text"] = "appair"
         
+        self.is_paired = False
+        
     def appair(self):
         self.status_frame.grid()
         self.power_scale.grid()
@@ -77,3 +129,5 @@ class Element(tk.Frame):
         self.posy_label.grid()
         self.date_label.grid()
         self.pair_button["text"] = "unappair"
+        
+        self.is_paired = True
